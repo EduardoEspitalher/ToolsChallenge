8c72f173df84b7bf987ebb2c0b9cd30e94a18ac6
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2025-06-16 15:05:22 -0300

    fix: usar @JsonProperty no enum PaymentType para suportar nomes com espaço no JSON

--- challenge/src/main/java/com/tools/challenge/enumeration/PaymentType.java
+++ challenge/src/main/java/com/tools/challenge/enumeration/PaymentType.java
@@ -1,8 +1,15 @@
 package com.tools.challenge.enumeration;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum PaymentType {
+    @JsonProperty("AVISTA")
     AVISTA,
+
+    @JsonProperty("PARCELADO LOJA")
     PARCELADO_LOJA,
+
+    @JsonProperty("PARCELADO EMISSOR")
     PARCELADO_EMISSOR
 
 }
